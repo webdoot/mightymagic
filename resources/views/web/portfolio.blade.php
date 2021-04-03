95f5746961ca558e5318b55145b5d6b437fd50a7
WEBDOOT <webdoot.com@gmail.com>
2021-04-03 10:57:30 +0530

    Portfolio page edited

--- resources/views/web/portfolio.blade.php
+++ resources/views/web/portfolio.blade.php
@@ -3,6 +3,16 @@
 @section('title', 'Portfolio')
 
 @push('head')
+<style type="text/css">
+    .ms_portfolio_inner .ms_p_item img {
+        width: 97%;
+    }
+
+    .ms_p_item img {
+        border: 2px solid #7e8d9c;
+        margin: 6px 0;
+    }
+</style>
 @endpush
 
 @section('content')
@@ -23,6 +33,7 @@
             <div class="container">
                 <div class="portfolio_filter">
                     <ul>
+                        <li class="active" data-filter=".all"><a href="#">All </a></li>
                         <li data-filter=".film"><a href="#">Film & OTT Platform </a></li>
                         <li data-filter=".brand"><a href="#">Brand identity</a></li>
                         <li data-filter=".web"><a href="#">Web Solution </a></li>
@@ -32,47 +43,57 @@
             </div>
             <div class="ms_portfolio_inner">
                 <!--  Brand  -->
-                <div class="ms_p_item wd_25 brand">
-                    <img src="{{ asset('image/portfolio/brand/aditya-ent_logo.jpg')}}" alt="">
-                </div>
-                <div class="ms_p_item wd_25 brand">
-                    <img src="{{ asset('image/portfolio/brand/BROCHURE.jpg')}}" alt="">
-                </div>
-                <div class="ms_p_item wd_50 brand">
-                    <img src="{{ asset('image/portfolio/brand/Genius-Brochure.jpg')}}" alt="">
-                </div>
-                <div class="ms_p_item wd_25 brand">
+                <div class="ms_p_item wd_25 brand all">
                     <img src="{{ asset('image/portfolio/brand/InvitationCard.jpg')}}" alt="">
                 </div>
-                <div class="ms_p_item wd_25 brand">
-                    <img src="{{ asset('image/portfolio/brand/gipl_logo.jpg')}}" alt="">
+                <div class="ms_p_item wd_25 brand all corporate">
+                    <img src="{{ asset('image/portfolio/brand/BROCHURE.jpg')}}" alt="">
                 </div>
-                <div class="ms_p_item wd_25 brand">
-                    <img src="{{ asset('image/portfolio/brand/Infinity Hunt cover.jpg')}}" alt="">
+                <div class="ms_p_item wd_50 brand all">
+                    <img src="{{ asset('image/portfolio/brand/Genius-Brochure.jpg')}}" alt="">
                 </div>
-                <div class="ms_p_item wd_50 brand">
+                <div class="ms_p_item wd_50 brand all">
                     <img src="{{ asset('image/portfolio/brand/genius-kids-brochure.jpg')}}" alt="">
                 </div>
-                <div class="ms_p_item wd_25 brand">
+                <div class="ms_p_item wd_25 brand all corporate">
+                    <img src="{{ asset('image/portfolio/brand/gipl_logo.jpg')}}" alt="">
+                </div>
+                <div class="ms_p_item wd_25 brand all corporate">
+                    <img src="{{ asset('image/portfolio/brand/aditya ent_logo.jpg')}}" alt="">
+                </div>
+                <div class="ms_p_item wd_25 brand all corporate">
+                    <img src="{{ asset('image/portfolio/brand/Infinity Hunt cover.jpg')}}" alt="">
+                </div>
+                <div class="ms_p_item wd_25 brand all">
                     <img src="{{ asset('image/portfolio/brand/KHUSHI DESIGN.jpg')}}" alt="">
                 </div>
-                <div class="ms_p_item wd_25 brand">
+                <div class="ms_p_item wd_25 brand all corporate">
                     <img src="{{ asset('image/portfolio/brand/Uttam Logo.jpg')}}" alt="">
                 </div>
-                <div class="ms_p_item wd_25 brand">
+                <div class="ms_p_item wd_25 brand all">
                     <img src="{{ asset('image/portfolio/brand/vcard.jpg')}}" alt="">
                 </div>
 
                 <!--  Film  -->
-                <div class="ms_p_item wd_25 film">
+                <div class="ms_p_item wd_50 film all">
                     <img src="{{ asset('image/portfolio/film/101.jpg')}}" alt="">
                 </div>
-                <div class="ms_p_item wd_50 film">
+                <div class="ms_p_item wd_25 film all">
                     <img src="{{ asset('image/portfolio/film/102.jpg')}}" alt="">
                 </div>
-                <div class="ms_p_item wd_25 film">
+                <div class="ms_p_item wd_50 film all">
                     <img src="{{ asset('image/portfolio/film/103.jpg')}}" alt="">
                 </div>
+
+                <!--  Film  -->
+                <div class="ms_p_item wd_25 web all">
+                    <img src="{{ asset('image/portfolio/web/adity-entr.jpg')}}" alt="">
+                </div> 
+
+                <!--  Blank  -->
+                <div class="ms_p_item wd_25 brand corporate web all">
+                    <img src="{{ asset('image/portfolio/blank.jpg')}}" alt="">
+                </div>               
             </div>
         </section>
         <!--================End Portfolio Area =================-->
